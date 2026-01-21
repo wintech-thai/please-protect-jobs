55539feb51596a7818e2623101453bf4cdc46b8f
pjames <pjames@gmail.com>
2026-01-22 06:51:25 +0700

    Implementing loader logic

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,17 @@ RUN apt-get update -y
 RUN apt-get install -y wget curl zip unzip apt-transport-https ca-certificates gnupg lsb-release 
 
 # Install kubectl
-RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-RUN chmod +x kubectl
-RUN mv kubectl /usr/local/bin
+#RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#RUN chmod +x kubectl
+#RUN mv kubectl /usr/local/bin
 
 # Install gcloud
-RUN curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-    | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
-    | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-RUN apt-get update -y && apt-get install google-cloud-cli -y
-RUN gcloud -v
+#RUN curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+#    | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+#RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+#    | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+#RUN apt-get update -y && apt-get install google-cloud-cli -y
+#RUN gcloud -v
 
 WORKDIR /scripts
 COPY scripts/ .
--- scripts/ioc-loader.rb
+++ scripts/ioc-loader.rb
@@ -6,6 +6,51 @@ require 'uri'
 require 'redis'
 require './utils'
 
+def load_cache_aggregate(dbConn, redisObj, aggrType)
+  puts("DEBUG : Start loading aggregated data [#{aggrType}] from Redis...\n")
+
+  cnt = 0
+  upsertCount = 0
+  redisObj.scan_each(match: "#{aggrType}!!*") do |key|
+      lastSeen = redisObj.get(key)
+
+      lastSeenStr = Time.at(ts).strftime("%Y-%m-%d %H:%M:%S")
+
+      namespace, oicType, dataSet, iocValue = key.split("!!")
+
+      cnt = cnt + 1
+      puts("DEBUG_00 : [#{cnt}] Loading [#{namespace}] [#{oicType}] [#{dataSet}] [#{iocValue}] [#{lastSeenStr}]\n")
+
+      upsertKey = "UPSERT_#{aggrType}:#{key}"
+      previousLastSeen = redisObj.get(upsertKey)
+
+      needUpsert = false
+      if (previousLastSeen.nil?)
+        # Never seen that data in cache
+        needUpsert = true
+      else
+        if (previousLastSeen != lastSeen)
+          # Need to call upsert
+          needUpsert = true
+        end
+      end
+
+needUpsert = true # ทดสอบให้ upsert ตลอดเวลาก่อน 
+
+      if (needUpsert)
+        #upsertData(dbConn, type, keyword, aggrCount, cnt)
+        upsertCount = upsertCount + 1
+
+        redisObj.setex(upsertKey, 86400, lastSeen)
+      end
+  end
+
+  puts("DEBUG : Done loading [#{aggrType}] readCount=[#{cnt}], upsertCount=[#{upsertCount}] to PostgreSQL\n")
+  return cnt
+end
+
+##### Main #####
+#####
 if File.exist?('env.rb')
   #Default environment variables
   require './env'
@@ -32,3 +77,8 @@ end
 puts("INFO : ### Connected to PostgreSQL [#{pgHost}] [#{pgDb}]")
 
 redis = Redis.new(host: redisHost, port: redisPort)
+
+puts("INFO - Starting program to load IOC aggregated data to PostgreSQL...")
+
+type = 'aggr_network_blacklist_dest_ip_v1'
+totalLoad = load_cache_aggregate(conn, redis, 'IOC_SEEN')
